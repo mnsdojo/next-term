05390f0fca762ce83901f3468148942d3ef5468e
sensei <manishkumar97430@gmail.com>
2024-09-19 03:53:50 -0400

    setup ws

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,5 @@
 import http from "node:http";
+import { spawn } from "node-pty";
 import { createServer } from "./server";
 
 import { WebSocketServer } from "ws";
@@ -9,10 +10,42 @@ const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
 wss.on("connection", (ws) => {
-  ws.on("message", (data) => {
-    ws.send(`echo : ${data}`);
+  const ptyProcess = spawn("bash", [], {
+    name: "xterm-color",
+    // cwd: process.env.HOME,
+    env: process.env,
+  });
+
+  ws.on("message", (message) => {
+    console.log(`receieved - ${message}`);
+
+    try {
+      const data = JSON.parse(message.toString());
+      switch (data.type) {
+        case "command":
+          ptyProcess.write(data.command + "\n");
+          break;
+        default:
+          console.error("Unknown message type: ", data.type);
+      }
+    } catch (error) {
+      console.error("Error parsing JSON:", error);
+    }
+  });
+
+  ws.on("close", () => {
+    console.log("Closed ws");
+  });
+  ptyProcess.onData((data) => {
+    console.log(data);
+    const message = JSON.stringify({
+      type: "data",
+      data,
+    });
+    ws.send(message);
   });
 });
+
 server.listen(port, () => {
   console.log(`Listening on port :${port}`);
 });
